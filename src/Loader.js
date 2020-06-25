62a26eb941ad5bf8936d7a3a1727dcdbdc77cbda
Anastasia Starodubtseva <anastasiast1605@gmail.com>
2020-06-25 18:32:57 +0700

    First commit

---
+++ src/Loader.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+export default () => <div style={{display: 'flex', justifyContent: 'center', margin: '8px'}}>
+<div className="lds-dual-ring"></div></div>
---
+++ src/Todo/TodoItem.js
@@ -0,0 +1,56 @@
+import React, {useContext} from 'react'
+import PropTypes from 'prop-types'
+import Context from '../context'
+
+const styles = {
+  li: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '8px 16px',
+    border: '1px solid #ccc',
+    borderRadius: '3px',
+    marginBottom: '8px'
+  },
+  input: {
+    marginRight: '8px'
+  }
+}
+
+
+function TodoItem ({todo, index, onChange}) {
+  const {removeTodo} = useContext(Context)
+  const classes = []
+
+  if (todo.complited) {
+    classes.push('done')
+  }
+
+  return (
+    <li style={styles.li}>
+      <span className={classes.join(' ')}>
+        <input
+          type='checkbox'
+          checked={todo.complited}
+          style={styles.input}
+          onChange={() => onChange(todo.id )}
+        />
+        <strong>{index +1}</strong>
+        &nbsp;
+        {todo.title}
+      </span>
+
+      <button className='rm' onClick={removeTodo.bind(null, todo.id)}>
+        &times;
+      </button>
+    </li>
+  )
+}
+
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  index: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+}
+
+export default  TodoItem
